60bc2ab4b2c928f5d87dd5b80699329a5988f10a
naveen <naveekumarnaveen188@gmail.com>
2023-02-14 17:47:22 +0530

    SS

---
+++ count.java
@@ -0,0 +1,17 @@
+class Count
+{
+   static void num(int a)
+   {  
+      int count=0;
+      for(int i=0;i<=a;i++)
+      {
+         count++;
+      }
+      System.out.println(count);
+   }
+   public static void main (String[]args)
+     {
+         num(15);
+		 
+      }
+}
